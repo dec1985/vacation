40068025f4fc15791a8ef83460bd67aeb06fc807
fred <fred@taou.com>
2016-02-14 12:15:56 +0800

    add git ignore and finish deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mod_wsgi.server',
     'vacation',
 )
 
@@ -100,4 +101,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(os.path.abspath('.'), 'static')
 STATIC_URL = '/static/'
