80b732caec44057c6a37beb6cf007b290b4e39f5
fred <fred@taou.com>
2016-02-15 11:27:59 +0800

    add icon

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('vacation.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
 ]
